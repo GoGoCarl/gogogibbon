9061fc3f16f238d43a60ea0b5336b0d894dca4c0
Carl Scott <carl.scott@solertium.com>
2013-05-08 16:50:02 -0400

    Support for class methods for common tasks.
    Added a couple class methods to help with common tasks.

---
+++ lib/gogogibbon/callbacks.rb
@@ -0,0 +1,39 @@
+module GoGoGibbon
+
+  class Callbacks
+
+    attr_accessor :options
+
+    def after_create record
+      run do
+        GoGoGibbon.subscribe record
+      end
+    end
+
+    def after_update record
+      if record.email_changed? || record.first_name_changed? || record.last_name_changed?
+        run do 
+          GoGoGibbon.update_subscription record.email_was, record
+        end
+      end
+    end
+
+    def after_destroy record
+      run do
+        GoGoGibbon.cancel_user record
+      end
+    end
+
+    def run &block
+      if @options[:thread] == true
+        Thread.new do
+          yield
+        end
+      else
+        yield
+      end
+    end
+
+  end
+
+end
